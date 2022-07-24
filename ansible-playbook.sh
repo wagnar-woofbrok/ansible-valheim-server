2913c0f79a393a0b16683de7c1c889a459b86bde
Bailey Malone <bamalone@protonmail.com>
2022-07-23 17:25:06 -0700

    Fixed ansible-playbook script by adding inventory file, fixed apt clean in packages tasks and clarified inventory group for valheim server.

--- ansible-playbook.sh
+++ ansible-playbook.sh
@@ -6,5 +6,7 @@ set -o allexport
 source "$(dirname "$0")/.env"
 set +o allexport
 
+echo $PRIVATE_SSH_KEY_FILE
+
 # Run (env configured) Ansible Playbook
-exec ansible-playbook playbook.yml
+exec ansible-playbook -i inventory.yml playbook.yml
